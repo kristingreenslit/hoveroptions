5a1cc761891bb863c2df5296d317d6b7f07cd12d
kristingreenslit <kristingreenslit@gmail.com>
2016-01-11 16:05:47 -0500

    create fade out effect

--- site.js
+++ site.js
@@ -1,22 +1,38 @@
 window.onload = function(){
 
- var allPhotos = document.getElementsByClassName('circles');
- var i;
-  for (i = 0; i < allPhotos.length; i++) {
-  	allPhotos[i].style.display = 'block';
-  };
+ // var allPhotos = document.getElementsByClassName('circles');
+ // var i;
+ //  for (i = 0; i < allPhotos.length; i++) {
+ //   allPhotos[i].style.display = 'block';
+ //  };
 
-  var photoOne = document.getElementById('circle1');
-  photoOne.addEventListener('mouseover', changePhoto);
-  photoOne.addEventListener('mouseout', originalPhoto)
+ //  var photoOne = document.getElementById('circle1');
+ //  photoOne.addEventListener('mouseover', changePhoto);
+ //  photoOne.addEventListener('mouseout', originalPhoto)
 
-  function changePhoto(){
-  	photoOne.style.backgroundImage = "url('https://unsplash.it/150/150')";
-  }
+ //  function changePhoto(){
+ //   photoOne.style.backgroundImage = "url('images/picture7.jpeg')";
+ //  }
 
-  function originalPhoto(){
-  	photoOne.style.backgroundImage = "url('images/picture1.jpeg')";
-  }
+ //  function originalPhoto(){
+ //   photoOne.style.backgroundImage = "url('images/picture1.jpeg')";
+ //  }
+
+ function fadeInEffect() {
+    var fadeTarget = document.getElementById("circle1");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 200);
+}
+
+document.getElementById("circle1").addEventListener('mouseover', fadeInEffect);
 
 };
 
