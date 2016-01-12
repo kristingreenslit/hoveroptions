53f2612399b6d32286ce079c30097a15d9aadbe7
kristingreenslit <kristingreenslit@gmail.com>
2016-01-12 00:15:45 -0500

    animate circles on page load

--- site.js
+++ site.js
@@ -1,38 +1,13 @@
-window.onload = function(){
+$(document).ready(function(){
 
- // var allPhotos = document.getElementsByClassName('circles');
- // var i;
- //  for (i = 0; i < allPhotos.length; i++) {
- //   allPhotos[i].style.display = 'block';
- //  };
+  $('.circles').animate({left:0,width:"100",top:0,height: "100"},1000);
 
- //  var photoOne = document.getElementById('circle1');
- //  photoOne.addEventListener('mouseover', changePhoto);
- //  photoOne.addEventListener('mouseout', originalPhoto)
+  $('.circles').mouseenter(function(){
+   $(this).css("background-image", "url(images/picture10.jpeg)");
+  });
 
- //  function changePhoto(){
- //   photoOne.style.backgroundImage = "url('images/picture7.jpeg')";
- //  }
+  $(".circles").mouseleave(function(){
+   $(this).css("background-image", "url(images/picture09.jpeg)");
+  });
 
- //  function originalPhoto(){
- //   photoOne.style.backgroundImage = "url('images/picture1.jpeg')";
- //  }
-
- function fadeInEffect() {
-    var fadeTarget = document.getElementById("circle1");
-    var fadeEffect = setInterval(function () {
-        if (!fadeTarget.style.opacity) {
-            fadeTarget.style.opacity = 1;
-        }
-        if (fadeTarget.style.opacity < 0.1) {
-            clearInterval(fadeEffect);
-        } else {
-            fadeTarget.style.opacity -= 0.1;
-        }
-    }, 200);
-}
-
-document.getElementById("circle1").addEventListener('mouseover', fadeInEffect);
-
-};
-
+});
