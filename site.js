2536d06df6f7a28c8cc1e9e4020de6721695546f
kristingreenslit <kristingreenslit@gmail.com>
2016-01-26 22:36:37 -0500

    complete mouse hover effects

--- site.js
+++ site.js
@@ -1,27 +1,15 @@
 $(document).ready(function() {
 
-  $(".circles img").animate({height:"150px",width:"150px"},700);
+  $(".circles img").animate({
+    height: "150px",
+    width: "150px" 
+  }, 700);
 
-  $('.front #circle06').hover(function () {
-      $( this ).fadeTo('slow', 0.1);
-    }, function(){
+  $('.front .circles').hover(function() {
+      $( this ).fadeTo('slow', 0);
+    }, function() {
       $( this ).fadeTo('slow', 1);
-    });
+  });
 
 });
-
-
-
-
-
-
-
-
-   // $('.front #circle06').mouseenter(function () {
-   //    $( this ).fadeTo('slow', 0.1);
-   //  });
-    
-   //  $('.front #circle06').mouseout(function () {
-   //    $( this ).fadeTo('slow', 1);
-   //  });
 
